19c29da89271c90fe01935c1ce7bcdda813b9593
francoalonso <f@playone.gg>
2024-10-27 10:47:12 -0300

    Fin Backend

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/db";
 import router from "./router";
+import cors, { CorsOptions } from "cors";
 
 async function connectDB() {
   try {
@@ -15,8 +16,19 @@ connectDB();
 
 const server = express();
 
+const corsOption: CorsOptions = {
+  origin: function (origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error en cors capo"));
+    }
+  },
+};
+
+server.use(cors(corsOption));
+
 server.use(express.json());
 server.use("/api/fratelli", router);
-
 
 export default server;
